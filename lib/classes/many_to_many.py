51eb010cc14111a1cd61b26108dab5ea53643484
adan.bashir@student.moringaschool.com <adan.bashir@student.moringaschool.com>
2024-06-26 10:14:49 +0300

    update code challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,12 +1,14 @@
 class Article:
-    articles_list = []
+    all = []
 
     def __init__(self, author, magazine, title):
+        self._author = None
+        self._magazine = None
         self._title = None
         self.author = author
         self.magazine = magazine
         self.title = title
-        Article.articles_list.append(self)
+        Article.all.append(self)
 
     @property
     def title(self):
@@ -14,34 +16,31 @@ class Article:
 
     @title.setter
     def title(self, new_title):
-        if not (5 <= len(new_title) <= 50):
-            raise ValueError("Title length must be between 5 and 50 characters")
-        self._title = new_title
+            if isinstance(new_title, str):
+                if 5 <= len(new_title) <= 50:
+                    self._title = new_title
+                else:
+                    ValueError("Title must be between 5 and 50 characters")
 
     @property
     def author(self):
         return self._author
-
-    @author.setter
-    def author(self, new_author):
-        if not isinstance(new_author, Author):
-            raise TypeError("Author must be an instance of the Author class")
-        self._author = new_author
-
+    
     @property
     def magazine(self):
         return self._magazine
 
+    @author.setter
+    def author(self, new_author):
+        self._author = new_author
+
     @magazine.setter
     def magazine(self, new_magazine):
-        if not isinstance(new_magazine, Magazine):
-            raise TypeError("Magazine must be an instance of the Magazine class")
-        self._magazine = new_magazine
+            self._magazine = new_magazine
 
 
 class Author:
     def __init__(self, name):
-        self._name = None
         self.name = name
 
     @property
@@ -50,12 +49,17 @@ class Author:
 
     @name.setter
     def name(self, new_name):
-        if not new_name:
-            raise ValueError("Name can't be empty")
-        self._name = new_name
+        if hasattr(self, "name"):
+            AttributeError("Name can't be changed")
+        else:
+            if isinstance(new_name, str):
+                if len(new_name):
+                    self._name = new_name
+                else:
+                    ValueError("Name must be longer than 0 characters")
 
     def articles(self):
-        return [article for article in Article.articles_list if article.author == self]
+        return [article for article in Article.all if self == article.author]
 
     def magazines(self):
         return list({article.magazine for article in self.articles()})
@@ -64,14 +68,12 @@ class Author:
         return Article(self, magazine, title)
 
     def topic_areas(self):
-        areas = list({magazine.category for magazine in self.magazines()})
-        return areas if areas else None
+        topic_areas = list({magazine.category for magazine in self.magazines()})
+        return topic_areas if topic_areas else None
 
 
 class Magazine:
     def __init__(self, name, category):
-        self._name = None
-        self._category = None
         self.name = name
         self.category = category
 
@@ -79,43 +81,48 @@ class Magazine:
     def name(self):
         return self._name
 
-    @name.setter
-    def name(self, new_name):
-        if not (2 <= len(new_name) <= 16):
-            raise ValueError("Name length must be between 2 and 16 characters")
-        self._name = new_name
-
     @property
     def category(self):
         return self._category
 
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str):
+            if 2 <= len(new_name) <= 16:
+                self._name = new_name
+            else:
+                ValueError("Name must be between 2 and 16 characters")
+
     @category.setter
     def category(self, new_category):
-        if not new_category:
-            raise ValueError("Category can't be empty")
-        self._category = new_category
+        if isinstance(new_category, str):
+            if len(new_category):
+                self._category = new_category
+            else:
+                ValueError("Category must be longer than 0 characters")
 
     def articles(self):
-        return [article for article in Article.articles_list if article.magazine == self]
+        return [article for article in Article.all if self == article.magazine]
 
     def contributors(self):
         return list({article.author for article in self.articles()})
 
     def article_titles(self):
-        titles = [article.title for article in self.articles()]
-        return titles if titles else None
+        article_titles = [article.title for article in self.articles()]
+        return article_titles if article_titles else None
 
     def contributing_authors(self):
-        author_counts = {}
+        authors = {}
+        list_of_authors = []
+
         for article in self.articles():
-            author = article.author
-            if author not in author_counts:
-                author_counts[author] = 0
-            author_counts[author] += 1
+            if article.author in authors:
+                authors[article.author] += 1
+            else:
+                authors[article.author] = 1
 
-        frequent_authors = []
-        for author in author_counts:
-            if author_counts[author] >= 2:
-                frequent_authors.append(author)
+        for author, count in authors.items():
+            if count >= 2:
+                list_of_authors.append(author)
 
-        return frequent_authors if frequent_authors else None
+        return list_of_authors if list_of_authors else None
